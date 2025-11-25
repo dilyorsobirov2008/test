e494841600683778313ccf899a5c60005198f4e2
dilyorsobirov2008 <dilyorsobirov2008@gmail.com>
2025-11-25 15:16:10 +0900

    ozgardi

---
+++ ni.py
@@ -0,0 +1,15 @@
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+bot = telebot.TeleBot("8510007013:AAG-7POxIC8waS5xO2lkTgXDiNS6qVTVDkQ")
+@bot.message_handler(commands=['start'])
+def start(message):
+    #
+    markup = InlineKeyboardMarkup()
+    b1 = InlineKeyboardButton(text="Поднятия уровня в одиночку", url="https://t.me/+vxrOVi2Kj2BmYmYy")
+    b2 = InlineKeyboardButton(text="Баскетбол Куроко", url="https://t.me/+upBBVC1VzstiZTBi")
+    b3 = InlineKeyboardButton(text="Атака Титанов", url="https://t.me/+tE6bHu5EfT0yODhi")
+    markup.add(b1)
+    markup.add(b2)
+    markup.add(b3)
+    bot.send_message(message.chat.id, "Какой аниме хотите посмотреть?", reply_markup=markup)
+bot.polling()
